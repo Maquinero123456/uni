17123886c9d2fa81ba42a834ef29472cdf6a6fd3
Maquinero123456 <jimenanavarrodavid@uma.es>
2021-10-09 00:54:37 +0200

    Falta fib

--- EDA/practica1Extra.hs
+++ EDA/practica1Extra.hs
@@ -1,5 +1,4 @@
 import Test.QuickCheck
-import GHC.Integer (Integer)
 
 esPrimo :: (Integral a) => a -> Bool
 esPrimo x | x > 0 = esPrimo' x 2
@@ -20,3 +19,60 @@ libreDeCuadrados' x y z | not (esPrimo y)  = libreDeCuadrados' x (y+1) z
                         | mod x y == 0 && y == z = False
                         | mod x y == 0 && y /= z = libreDeCuadrados' (div x y) y y
                         | otherwise = libreDeCuadrados' x (y+1) z
+
+sumaDigitos :: Integer -> Integer
+sumaDigitos 0 = 0
+sumaDigitos x | x>0= mod x 10 + sumaDigitos (div x 10)
+              | otherwise = error "Argumento no natural"
+
+harshad :: Integer -> Bool
+harshad x | x<=0 = error "Argumento no valido"
+          | mod x (sumaDigitos x) == 0 = True
+          | otherwise= False
+
+multipleHarshad :: Integer -> Bool
+multipleHarshad 1 = True
+multipleHarshad x | x<=0 = error "Argumento no valido"
+                  | harshad x = multipleHarshad (div x (sumaDigitos x))
+                  | otherwise= False
+
+vecesHarshad :: Integer -> Integer
+vecesHarshad 1 = 1
+vecesHarshad x | x<=0 = error "Argumento no valido"
+               | harshad x && sumaDigitos x == 1 = 1
+               | harshad x = 1 + vecesHarshad (div x (sumaDigitos x))
+               | otherwise= 0
+
+prop_Boem_Harshad_OK x = x>0 ==> vecesHarshad (1008*10^x) == veces
+    where
+        veces = x+2
+
+cerosDe :: Integer -> Integer
+cerosDe 0 = 1
+cerosDe x | mod x 10 == 0 = 1 + cerosDe (div x 10)
+          | otherwise = 0
+
+prop_cerosDe_Ok n m = m > 0 && m < 1000 ==> cerosDe construido == m
+    where construido = n*10^m
+
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib x | x<0 = error "Argumento negativo"
+      | otherwise = fib(x-1)+fib(x-2)
+
+llamadasFib :: Integer -> Integer
+llamadasFib 0 = 1
+llamadasFib 1 = 1
+llamadasFib x | x<0 = error "Argumento negativo"
+              | otherwise= 1+llamadasFib(x-1)+llamadasFib(x-2)
+
+fib' :: Integer -> Integer
+fib' n = fibAc n 0 1
+    where
+        fibAc = undefined
+
+--Add fib=fib'--
+
+binet :: Integer -> Integer
+binet x = undefined
