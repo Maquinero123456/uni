43fba6be632b210da33500b7c185803cd313c4cd
Maquinero123456 <jimenanavarrodavid@uma.es>
2021-11-25 05:45:43 +0100

    Poco

--- EDA/Tema 3/haskell/wellBalanced.hs
+++ EDA/Tema 3/haskell/wellBalanced.hs
@@ -7,5 +7,5 @@ wellBalanced xs = wellBalanced' xs S.empty
 wellBalanced' :: String -> Stack Char -> Bool
 wellBalanced' [] s = isEmpty s
 wellBalanced' (x:xs) s | x == '{' || x == '[' || x == '(' = wellBalanced' xs (push x s)
-                       | x == '}' && top s == '{' || x == ']' && top s == '[' || x == ')' && (top s) == '(' = wellBalanced' xs (pop s)
+                       | (x == '{' || x == '[' || x == '(') && top s == x  = wellBalanced' xs (pop s)
                        | otherwise = wellBalanced' xs s
