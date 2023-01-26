107197783ae3493c11287d98f6034fc6c204294b
Maquinero123456 <jimenanavarrodavid@uma.es>
2023-01-26 06:50:35 +0100

    ProyetosTema4

--- desSerTel/tema4/spotifyplaylist/lib/spotify.js
+++
@@ -1,8 +0,0 @@
-import SpotifyWebApi from "spotify-web-api-node";
-
-const spotifyWebApi = new SpotifyWebApi({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-})
-
-export default spotifyWebApi;
--- desSerTel/tema4/spotifyplaylist/pages/api/getToken.js
+++
@@ -1,8 +0,0 @@
-import {getAccessToken} from '../../lib/spotify';
-import {getSession} from 'next-auth/react';
-
-const handler = async (req, res) => {
-  return await getAccessToken;
-};
-
-export default handler;
