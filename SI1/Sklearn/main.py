6d77851df05e263e6193ab9e8051097c8ee6f9fc
Maquinero123456 <jimenanavarrodavid@gmail.com>
2024-06-02 23:00:17 +0200

    sklearn

--- SI1/Sklearn/main.py
+++ SI1/Sklearn/main.py
@@ -8,8 +8,7 @@ from sklearn.svm import SVC
 import matplotlib.pyplot as plt
 from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import LabelBinarizer
-from sklearn.metrics import RocCurveDisplay, roc_auc_score
-from sklearn.model_selection import cross_val_score
+from sklearn.metrics import RocCurveDisplay
 from itertools import cycle
 from sklearn.metrics import auc, roc_curve
 
@@ -26,6 +25,7 @@ columnas = ["Id_number", "RI", "Na", "Mg", "Al", "Si", "K", "Ca", "Ba", "Fe", "T
 
 df = pd.read_csv('data/glass.data', names = columnas)
 df = df.drop('Id_number', axis=1)
+print("Dataframe:")
 print(df)
 
 #Dataframe como X e Y
@@ -33,25 +33,17 @@ x = df.iloc[:, :8].values
 y = df.iloc[:, 9].values
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 skf = StratifiedKFold(n_splits=10)
-valores = skf.split(x, y)
-
 
 #Bayes
+print("################## Naive Bayes Gaussiano ##################")
 gnb = GaussianNB()
-y_pred = gnb.fit(x_train, y_train).predict(x_test)
-print("Number of mislabeled points out of a total %d points : %d" % (x_test.shape[0], (y_test != y_pred).sum()))
+gnb = gnb.fit(x_train, y_train)
+y_pred = gnb.predict(x_test)
+print("Cantidad de predicciones mal hechas de %d valores : %d" % (x_test.shape[0], (y_test != y_pred).sum()))
+print("Accuracy "+str((y_test != y_pred).sum()*100/x_test.shape[0])+"%")
 
 #KNeighbours
-#bestNNeigh = 0
-#best_pred = 0 
-#for i in range(1, 100):
-#    neigh = KNeighborsClassifier(n_neighbors=i)
-#    neigh.fit(x_train, y_train)
-#    y_pred = (y_test == neigh.predict(x_test)).sum()
-#    if y_pred > best_pred:
-#        bestNNeigh = i
-#        best_pred = y_pred
-#print("Mejor cantidad de vecinos: ", bestNNeigh)
+print("################## Nearest Neighbor ##################")
 gsVecinos = GridSearchCV(
     KNeighborsClassifier(),
     param_grid={"n_neighbors":range(1,101)},
@@ -63,10 +55,10 @@ print("El mejor numero de vecinos es: ",gsVecinos.best_params_['n_neighbors'])
 print("Mejor precision: ", gsVecinos.best_score_)
 vecinosMejor = gsVecinos.best_estimator_
 y_pred = vecinosMejor.predict(x_test)
-print("Cantidad de preddicciones mal hechas de %d valores : %d" % (x_test.shape[0], (y_test != y_pred).sum()))
+print("Cantidad de predicciones mal hechas de %d valores : %d" % (x_test.shape[0], (y_test != y_pred).sum()))
 
 #Decision Tree
-#Mejor: 0.7248366013071895
+print("################## Decision Tree ##################")
 parametros = {
     "criterion" : ["gini", "entropy", "log_loss"],
     "splitter" : ["best", "random"],
@@ -91,11 +83,11 @@ treeMejor = gs.best_estimator_
 y_pred = treeMejor.predict(x_test)
 print("Cantidad de predicciones mal hechas de %d valores : %d" % (x_test.shape[0], (y_test != y_pred).sum()))
 
-#TODO quitar comentarios
-#plot_tree(treeMejor)
-#plt.show()
+plot_tree(treeMejor)
+plt.show()
 
 #Support Vector Machine
+print("################## SVC ##################")
 parametrosSVC = {
     "kernel" : ['poly'],
     "gamma" : ['scale', 'auto'],
@@ -116,10 +108,11 @@ print("Mejor parametros:", gsSVC.best_params_)
 print("Mejor precisión: ", gsSVC.best_score_)
 svcMejor = gsSVC.best_estimator_
 y_pred = svcMejor.predict(x_test)
-print("Cantidad de preddicciones mal hechas de %d valores : %d" % (x_test.shape[0], (y_test != y_pred).sum()))
+print("Cantidad de predicciones mal hechas de %d valores : %d" % (x_test.shape[0], (y_test != y_pred).sum()))
 
-#Metricas
-y_score_Gaussian = GaussianNB().fit(x_train, y_train).predict_proba(x_test)
+#Graficas
+print("################## Graficas ##################")
+y_score_Gaussian = gnb.predict_proba(x_test)
 y_score_vecinos = vecinosMejor.predict_proba(x_test) 
 y_score_tree = treeMejor.predict_proba(x_test)
 y_score_svc = svcMejor.predict_proba(x_test)
@@ -129,124 +122,119 @@ target_names = [1,2,3,4,5,6,7]
 label_binarizer = LabelBinarizer().fit(y_train)
 y_onehot_test = label_binarizer.transform(y_test)
 n_classes = y_onehot_test.shape[1]
-class_of_interest = 1
 colors = cycle(["aqua", "darkorange", "cornflowerblue", "green", "blue", "yellow", "red", "brown"])
 
-#Gaussian Metrica
-fpr, tpr, roc_auc = dict(), dict(), dict()
-for i in range(n_classes):
-    fpr[i], tpr[i], _ = roc_curve(y_onehot_test[:, i], y_score_Gaussian[:, i])
-    roc_auc[i] = auc(fpr[i], tpr[i])
+def plots(y_score, y_onehot_test, n_classes, texto):
+    colors = cycle(["aqua", "darkorange", "cornflowerblue", "green", "blue", "yellow", "red", "brown"])
+    #Gaussian Metrica
+    fpr, tpr, roc_auc = dict(), dict(), dict()
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_onehot_test[:, i], y_score[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
 
-fpr_grid = np.linspace(0.0, 1.0, 1000)
+    fpr_grid = np.linspace(0.0, 1.0, 1000)
 
-# Interpolate all ROC curves at these points
-mean_tpr = np.zeros_like(fpr_grid)
+    # Interpolate all ROC curves at these points
+    mean_tpr = np.zeros_like(fpr_grid)
 
-for i in range(n_classes):
-    mean_tpr += np.interp(fpr_grid, fpr[i], tpr[i])  # linear interpolation
+    for i in range(n_classes):
+        mean_tpr += np.interp(fpr_grid, fpr[i], tpr[i])  # linear interpolation
 
-# Average it and compute AUC
-mean_tpr /= n_classes
+    # Average it and compute AUC
+    mean_tpr /= n_classes
 
-fpr["macro"] = fpr_grid
-tpr["macro"] = mean_tpr
-roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+    fpr["macro"] = fpr_grid
+    tpr["macro"] = mean_tpr
+    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
 
-fpr["micro"], tpr["micro"], _ = roc_curve(y_onehot_test.ravel(), y_score_Gaussian.ravel())
-roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_onehot_test.ravel(), y_score_Gaussian.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
 
-fig, ax = plt.subplots(figsize=(6, 6))
-plt.plot(
-    fpr["micro"],
-    tpr["micro"],
-    label=f"micro-average ROC curve (AUC = {roc_auc['micro']:.2f})",
-    color="deeppink",
-    linestyle=":",
-    linewidth=4,
-)
-plt.plot(
-    fpr["macro"],
-    tpr["macro"],
-    label=f"macro-average ROC curve (AUC = {roc_auc['macro']:.2f})",
-    color="navy",
-    linestyle=":",
-    linewidth=4,
-)
-for class_id, color in zip(range(n_classes), colors):
+    fig, ax = plt.subplots(figsize=(10, 10))
+    plt.plot(
+        fpr["micro"],
+        tpr["micro"],
+        label=f"micro-average ROC curve (AUC = {roc_auc['micro']:.2f})",
+        color="deeppink",
+        linestyle=":",
+        linewidth=4,
+    )
+    plt.plot(
+        fpr["macro"],
+        tpr["macro"],
+        label=f"macro-average ROC curve (AUC = {roc_auc['macro']:.2f})",
+        color="navy",
+        linestyle=":",
+        linewidth=4,
+    )
+    for class_id, color in zip(range(n_classes), colors):
+        RocCurveDisplay.from_predictions(
+            y_onehot_test[:, class_id],
+            y_score[:, class_id],
+            name=f"ROC curve for {target_names[class_id]}",
+            color=color,
+            ax=ax,
+            plot_chance_level=(class_id == 2),
+        )
+
+    _ = ax.set(
+        xlabel="False Positive Rate",
+        ylabel="True Positive Rate",
+        title="Extension of Receiver Operating Characteristic\nto One-vs-Rest multiclass("+texto+")",
+    )
+    plt.tight_layout()
+    plt.show()
+
+#Gaussian
+plots(y_score_Gaussian, y_onehot_test, n_classes, "Naive Bayes Gaussiano")
+#Vecinos
+plots(y_score_vecinos, y_onehot_test, n_classes, "Nearest Neighbor")
+#Tree
+plots(y_score_tree, y_onehot_test, n_classes, "Decision Tree")
+#SVC
+plots(y_score_svc, y_onehot_test, n_classes, "SVC")
+
+#Metricas comparado un par de valores
+def plot_comparar(class_id, y_score_gaussian, y_score_vecinos, y_score_tree, y_score_svc, y_onehot_test):
+    fig, ax = plt.subplots(figsize=(6, 6))
     RocCurveDisplay.from_predictions(
         y_onehot_test[:, class_id],
-        y_score_Gaussian[:, class_id],
-        name=f"ROC curve for {target_names[class_id]}",
-        color=color,
-        ax=ax,
-        plot_chance_level=(class_id == 2),
+        y_score_gaussian[:, class_id],
+        name="Gaussian",
+        color="red",
+        plot_chance_level=True,
+        ax=ax
+    )
+    _ = ax.set(
+        xlabel="False Positive Rate",
+        ylabel="True Positive Rate",
+        title="Naive Bayes Gaussiano vs Nearest Neighbor vs Decision Tree vs SVC("+str(class_id)+")",
     )
 
-_ = ax.set(
-    xlabel="False Positive Rate",
-    ylabel="True Positive Rate",
-    title="Extension of Receiver Operating Characteristic\nto One-vs-Rest multiclass",
-)
-plt.show()
-
-
-#Vecinos Metrica
-fig, ax = plt.subplots(figsize=(6, 6))
-for class_id, color in zip(range(n_classes), colors):
     RocCurveDisplay.from_predictions(
         y_onehot_test[:, class_id],
         y_score_vecinos[:, class_id],
-        name=f"ROC curve for {target_names[class_id]}",
-        color=color,
-        ax=ax,
-        plot_chance_level=(class_id == 2),
+        name="Vecinos",
+        color="darkorange",
+        ax=ax
     )
 
-_ = ax.set(
-    xlabel="False Positive Rate",
-    ylabel="True Positive Rate",
-    title="Extension of Receiver Operating Characteristic\nto One-vs-Rest multiclass",
-)
-plt.show()
-
-
-#tree Metrica
-fig, ax = plt.subplots(figsize=(6, 6))
-for class_id, color in zip(range(n_classes), colors):
     RocCurveDisplay.from_predictions(
         y_onehot_test[:, class_id],
         y_score_tree[:, class_id],
-        name=f"ROC curve for {target_names[class_id]}",
-        color=color,
-        ax=ax,
-        plot_chance_level=(class_id == 2),
+        name="Tree",
+        color="blue",
+        ax=ax
     )
 
-_ = ax.set(
-    xlabel="False Positive Rate",
-    ylabel="True Positive Rate",
-    title="Extension of Receiver Operating Characteristic\nto One-vs-Rest multiclass",
-)
-plt.show()
-
-
-#svc Metrica
-fig, ax = plt.subplots(figsize=(6, 6))
-for class_id, color in zip(range(n_classes), colors):
     RocCurveDisplay.from_predictions(
         y_onehot_test[:, class_id],
         y_score_svc[:, class_id],
-        name=f"ROC curve for {target_names[class_id]}",
-        color=color,
-        ax=ax,
-        plot_chance_level=(class_id == 2),
+        name="SVC",
+        color="green",
+        ax=ax
     )
+    plt.show()
 
-_ = ax.set(
-    xlabel="False Positive Rate",
-    ylabel="True Positive Rate",
-    title="Extension of Receiver Operating Characteristic\nto One-vs-Rest multiclass",
-)
-
-plt.show()
+plot_comparar(1, y_score_Gaussian, y_score_vecinos, y_score_tree, y_score_svc, y_onehot_test)
+plot_comparar(0, y_score_Gaussian, y_score_vecinos, y_score_tree, y_score_svc, y_onehot_test)
