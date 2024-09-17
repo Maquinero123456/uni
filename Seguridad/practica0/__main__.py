d3b8e7c26382bcb262502bf0b2173b304b4377ab
Maquinero123456 <jimenanavarrodavid@gmail.com>
2024-09-17 17:19:25 +0200

    Practica0 seguridad

---
+++ Seguridad/practica0/__main__.py
@@ -0,0 +1,155 @@
+def cifradoCesarAlfabetoInglesMAY(cadena):
+	"""Devuelve	un	cifrado	Cesar	tradicional	(+3)"""
+	#	Definir	la	nueva	cadena	resultado
+	resultado	=	''
+	#	Realizar	el	"cifrado",	sabiendo	que	A	=	65,	Z	=	90,	a	=	97,	z	=	122
+	i	=	0
+	while i	< len(cadena):
+		#	Recoge	el	caracter	a	cifrar
+		ordenClaro	=	ord(cadena[i])
+		ordenCifrado	=	0
+		#	Cambia	el	caracter	a	cifrar
+		if (ordenClaro	>=	65 and ordenClaro	<=	90):
+			ordenCifrado	=	(((ordenClaro	- 65)	+	3)	%	26)	+	65
+			#	Añade	el	caracter	cifrado	al	resultado
+			resultado	=	resultado	+	chr(ordenCifrado)
+			i	=	i	+	1
+			#	devuelve	el	resultado
+	return resultado
+
+def descifradoCesarAlfabetoInglesMAY(cadena):
+	"""Devuelve	un	descifrado	Cesar	tradicional	(-3)"""
+	#	Definir	la	nueva	cadena	resultado
+	resultado	=	''
+	#	Realizar	el	"cifrado",	sabiendo	que	A	=	65,	Z	=	90,	a	=	97,	z	=	122
+	i	=	0
+	while i	< len(cadena):
+		#	Recoge	el	caracter	a	cifrar
+		ordenClaro	=	ord(cadena[i])
+		ordenCifrado	=	0
+		#	Cambia	el	caracter	a	cifrar
+		if (ordenClaro	>=	65 and ordenClaro	<=	90):
+			ordenCifrado	=	(((ordenClaro - 65)	- 3) % 26) + 65
+			#	Añade	el	caracter	cifrado	al	resultado
+			resultado = resultado + chr(ordenCifrado)
+			i = i	+ 1
+			#	devuelve	el	resultado
+	return resultado
+
+def cifradoCesarAlfabetoIngles(cadena):
+	"""Devuelve	un	cifrado	Cesar	tradicional	(+3)"""
+	#	Definir	la	nueva	cadena	resultado
+	resultado	=	''
+	#	Realizar	el	"cifrado",	sabiendo	que	A	=	65,	Z	=	90,	a	=	97,	z	=	122
+	i	=	0
+	while i	< len(cadena):
+		#	Recoge	el	caracter	a	cifrar
+		ordenClaro	=	ord(cadena[i])
+		ordenCifrado	=	0
+		#	Cambia	el	caracter	a	cifrar
+		if (ordenClaro	>=	65 and ordenClaro	<=	90):
+			ordenCifrado	=	(((ordenClaro	- 65)	+	3)	%	26)	+	65
+			#	Añade	el	caracter	cifrado	al	resultado
+			resultado	=	resultado	+	chr(ordenCifrado)
+			i	=	i	+	1
+			#	devuelve	el	resultado
+		else:
+			ordenCifrado	=	(((ordenClaro	- 97)	+	3)	%	26)	+	97
+			#	Añade	el	caracter	cifrado	al	resultado
+			resultado	=	resultado	+	chr(ordenCifrado)
+			i	=	i	+	1
+			#	devuelve	el	resultado
+	return resultado
+
+def descifradoCesarAlfabetoIngles(cadena):
+	"""Devuelve	un	descifrado	Cesar	tradicional	(-3)"""
+	#	Definir	la	nueva	cadena	resultado
+	resultado	=	''
+	#	Realizar	el	"cifrado",	sabiendo	que	A	=	65,	Z	=	90,	a	=	97,	z	=	122
+	i	=	0
+	while i	< len(cadena):
+		#	Recoge	el	caracter	a	cifrar
+		ordenClaro	=	ord(cadena[i])
+		ordenCifrado	=	0
+		#	Cambia	el	caracter	a	cifrar
+		if (ordenClaro	>=	65 and ordenClaro	<=	90):
+			ordenCifrado	=	(((ordenClaro - 65)	- 3) % 26) + 65
+			#	Añade	el	caracter	cifrado	al	resultado
+			resultado = resultado + chr(ordenCifrado)
+			i = i	+ 1
+			#	devuelve	el	resultado
+		else:
+			ordenCifrado	=	(((ordenClaro	- 97) - 3)	%	26)	+	97
+			#	Añade	el	caracter	cifrado	al	resultado
+			resultado	=	resultado	+	chr(ordenCifrado)
+			i	=	i	+	1
+			#	devuelve	el	resultado
+	return resultado
+
+def cifradoCesarAlfabetoInglesI(cadena, desplazamiento):
+	"""Devuelve	un	cifrado	Cesar	tradicional	(+3)"""
+	#	Definir	la	nueva	cadena	resultado
+	resultado	=	''
+	#	Realizar	el	"cifrado",	sabiendo	que	A	=	65,	Z	=	90,	a	=	97,	z	=	122
+	i	=	0
+	while i	< len(cadena):
+		#	Recoge	el	caracter	a	cifrar
+		ordenClaro	=	ord(cadena[i])
+		ordenCifrado	=	0
+		#	Cambia	el	caracter	a	cifrar
+		if (ordenClaro	>=	65 and ordenClaro	<=	90):
+			ordenCifrado	=	(((ordenClaro	- 65)	+	desplazamiento)	%	26)	+	65
+			#	Añade	el	caracter	cifrado	al	resultado
+			resultado	=	resultado	+	chr(ordenCifrado)
+			i	=	i	+	1
+			#	devuelve	el	resultado
+		else:
+			ordenCifrado	=	(((ordenClaro	- 97)	+	desplazamiento)	%	26)	+	97
+			#	Añade	el	caracter	cifrado	al	resultado
+			resultado	=	resultado	+	chr(ordenCifrado)
+			i	=	i	+	1
+			#	devuelve	el	resultado
+	return resultado
+
+def descifradoCesarAlfabetoInglesI(cadena, desplazamiento):
+	"""Devuelve	un	descifrado	Cesar	tradicional	(-3)"""
+	#	Definir	la	nueva	cadena	resultado
+	resultado	=	''
+	#	Realizar	el	"cifrado",	sabiendo	que	A	=	65,	Z	=	90,	a	=	97,	z	=	122
+	i	=	0
+	while i	< len(cadena):
+		#	Recoge	el	caracter	a	cifrar
+		ordenClaro	=	ord(cadena[i])
+		ordenCifrado	=	0
+		#	Cambia	el	caracter	a	cifrar
+		if (ordenClaro	>=	65 and ordenClaro	<=	90):
+			ordenCifrado	=	(((ordenClaro - 65)	- desplazamiento) % 26) + 65
+			#	Añade	el	caracter	cifrado	al	resultado
+			resultado = resultado + chr(ordenCifrado)
+			i = i	+ 1
+			#	devuelve	el	resultado
+		else:
+			ordenCifrado	=	(((ordenClaro	- 97) - desplazamiento)	%	26)	+	97
+			#	Añade	el	caracter	cifrado	al	resultado
+			resultado	=	resultado	+	chr(ordenCifrado)
+			i	=	i	+	1
+			#	devuelve	el	resultado
+	return resultado
+
+
+
+def main():
+    palabra = "HOLA"
+    cifradoMayusculas = cifradoCesarAlfabetoInglesMAY(palabra)
+    print(cifradoMayusculas)
+    print(descifradoCesarAlfabetoInglesMAY(cifradoMayusculas))
+    cifradoNormal = cifradoCesarAlfabetoIngles(palabra)
+    print(cifradoNormal)
+    print(descifradoCesarAlfabetoIngles(cifradoNormal))
+    cifradoI = cifradoCesarAlfabetoInglesI(palabra, 6)
+    print(cifradoI)
+    print(descifradoCesarAlfabetoInglesI(cifradoI, 6))
+	
+
+if __name__ == "__main__":
+    main()
